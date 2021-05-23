b733b08edf0d34eb1685922dfddd5d44e0793b49
jorbuedo <jorbuedo@me.com>
2021-05-23 15:20:06 +0200

    ToggleFavorites with optimistic UI

--- components/Favorite/Favorite.tsx
+++ components/Favorite/Favorite.tsx
@@ -1,3 +1,8 @@
+import {
+  setFavoriteGrocery,
+  getFavoriteOptimisticOptions,
+} from 'models/Grocery'
+import { useMutation, useQueryClient } from 'react-query'
 import { tw } from 'twind/shim'
 import Heart from './Heart'
 
@@ -7,15 +12,22 @@ type FavoriteProps = {
 }
 
 export default function Favorite({ isFavorite, id }: FavoriteProps) {
+  const queryClient = useQueryClient()
+
+  const { mutate: setFavorite, status } = useMutation(
+    setFavoriteGrocery,
+    getFavoriteOptimisticOptions(queryClient),
+  )
+
   const handleClick = () => {
-    console.log({ id })
+    setFavorite({ id, isFavorite: !isFavorite })
   }
 
   return (
     <button
       className={tw`p-2 rounded outline-none focus-visible:ring-2 hover:animate-pulse ${
         isFavorite ? 'text-red-600' : 'text-white'
-      }`}
+      } ${status === 'loading' && 'animate-ping'}`}
       type="button"
       onClick={handleClick}
       aria-label={isFavorite ? 'Remove from favorites' : 'Add to favorites'}
--- models/Grocery.ts
+++ models/Grocery.ts
@@ -1,3 +1,5 @@
+import produce from 'immer'
+import { QueryClient, UseMutationOptions } from 'react-query'
 import { api } from 'utils/constants'
 
 export type Grocery = {
@@ -12,6 +14,9 @@ export type Grocery = {
 
 export const getGroceries = async () => {
   const res = await fetch(`${api}/grocery`)
+  if (!res.ok) {
+    throw new Error('Network response was not ok')
+  }
   return res.json()
 }
 
@@ -23,10 +28,94 @@ export const getGrocery = async ({
 }) => {
   const [, { id }] = queryKey
   const res = await fetch(`${api}/grocery/${id}`)
+  if (!res.ok) {
+    throw new Error('Network response was not ok')
+  }
   return res.json()
 }
 
 export const getFavoriteGroceries = async () => {
   const res = await fetch(`${api}/grocery?favorite=true`)
+  if (!res.ok) {
+    throw new Error('Network response was not ok')
+  }
   return res.json()
 }
+
+type FavoriteProps = {
+  id: Grocery['id']
+  isFavorite: Grocery['favorite']
+}
+
+export const setFavoriteGrocery = async ({
+  id,
+  isFavorite,
+}: FavoriteProps): Promise<Grocery> => {
+  const res = await fetch(`${api}/grocery/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ favorite: isFavorite }),
+  })
+  if (!res.ok) {
+    throw new Error('Network response was not ok')
+  }
+  return res.json()
+}
+
+export const getFavoriteOptimisticOptions: (
+  queryClient: QueryClient,
+) => UseMutationOptions<Grocery, Error, FavoriteProps, Grocery | undefined> = (
+  queryClient,
+) => ({
+  onMutate: async ({ isFavorite, id }) => {
+    await queryClient.cancelQueries('groceryList')
+    const groceryList = queryClient.getQueryData<Grocery[]>('groceryList')
+
+    if (groceryList === undefined) {
+      return undefined
+    }
+
+    const groceryIndex = groceryList?.findIndex((g) => id === g.id)
+
+    if (groceryIndex === undefined || groceryIndex === -1) {
+      return undefined
+    }
+    queryClient.setQueryData<Grocery[]>(
+      'groceryList',
+      produce(groceryList, (draft) => {
+        // eslint-disable-next-line no-param-reassign
+        draft[groceryIndex].favorite = isFavorite
+      }),
+    )
+
+    return groceryList[groceryIndex]
+  },
+  onError: (err, { id }, previousGrocery) => {
+    // eslint-disable-next-line no-console
+    console.error('Failed to set favorite: ', id, err)
+    if (previousGrocery) {
+      const groceryList = queryClient.getQueryData<Grocery[]>('groceryList')
+      const groceryIndex = groceryList?.findIndex((g) => id === g.id)
+
+      if (
+        groceryList !== undefined &&
+        groceryIndex !== undefined &&
+        groceryIndex >= 0
+      ) {
+        queryClient.setQueryData<Grocery[]>(
+          'groceryList',
+          produce(groceryList, (draft) => {
+            // eslint-disable-next-line no-param-reassign
+            draft[groceryIndex].favorite = previousGrocery.favorite
+          }),
+        )
+      }
+    }
+  },
+  onSettled: () => {
+    queryClient.invalidateQueries('groceryList')
+    queryClient.invalidateQueries('favoriteList')
+  },
+})
