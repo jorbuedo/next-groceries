520fb4fd73faab592e5117266412eacb72220273
jorbuedo <jorbuedo@me.com>
2021-05-30 16:40:21 +0200

    Add heroku api

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,4 @@
 export const api =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : '/api'
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3001'
+    : 'https://next-groceries-db.herokuapp.com'
